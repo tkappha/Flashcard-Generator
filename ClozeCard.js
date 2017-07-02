62796decc2ba50380c815b4782d75a13901fb69c
Tami <tkapphahn@gmail.com>
2017-07-01 20:01:59 -0700

    comment

---
+++ ClozeCard.js
@@ -0,0 +1,14 @@
+//**********************************************************************
+// Create a new file named ClozeCard.js:
+// ---------------------------------------
+
+// This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
+// module.exports = ClozeCard;
+
+// The constructor should accept two arguments: text and cloze.
+// The constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
+// The constructed object should have a partial property that contains only the partial text.
+// The constructed object should have a fullText property that contains only the full text.
+// The constructor should throw or log an error when the cloze deletion does not appear in the input text.
+// Use prototypes to attach these methods, wherever possible.
+//*************************************************************************
